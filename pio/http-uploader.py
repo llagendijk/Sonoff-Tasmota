a6bef3285bc5a1c4caee2e7b7fab4a181fa3a474
Louis Lagendijk <louis.lagendijk@gmail.com>
2019-03-02 12:57:16 +0100

    Push own patches for platformio + merge script

--- pio/http-uploader.py
+++ pio/http-uploader.py
@@ -2,7 +2,7 @@ Import("env")
 
 from base64 import b64decode
 
-env.Replace(UPLOADER="pio\espupload.py")
+env.Replace(UPLOADER="pio/espupload.py")
 env.Replace(UPLOADERFLAGS="")
 env.Replace(UPLOADCMD="$UPLOADER -u " + b64decode(ARGUMENTS.get("UPLOAD_PORT")) + " -f $SOURCES")
 
